c5fc79964f5ef9a8786693089d7a6254455f9702
StefanLamminpija <stefan.lamminpija@tptlive.ee>
2024-10-01 15:36:58 +0300

    Start

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> SchoolRate </title>
 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">
@@ -23,7 +25,7 @@
             font-optical-sizing: auto;
             font-weight: 400;
             font-style: normal;
-            font-size: 10px;
+            font-size: 25px;
             text-align: center;
             margin: 60px;
 
@@ -31,6 +33,8 @@
 
         .second_heading {
 
+            display: flex;
+            justify-content: space-around;
             font-family: "Manrope", sans-serif;
             font-optical-sizing: auto;
             font-weight: 400;
@@ -38,70 +42,275 @@
             font-variation-settings:
             "YOPQ" 300;
             font-size: 16px;
-            text-align: center;
-            word-spacing: 64px;
-            padding-left: 215px;
+            padding: 5px;
+            text-align: left;
+            margin-left: 570px;
+            margin-right: 400px;
+            border: 0.5px;
+            border-style: solid;
+            border-radius: 3px;
+
         }
 
-        .math {
+        .math_box {
 
+            display: flex;
+            justify-content: space-around;
             font-family: "Kumbh Sans", sans-serif;
             font-optical-sizing: auto;
             font-weight: 500;
             font-style: normal;
             font-variation-settings:
             "YOPQ" 300;
-            text-align: center;
-            word-spacing: 80px;
+            text-align: left;
             border: 0.5px;
             border-style: solid;
             padding: 20px;
             margin-top: 10px;
-            margin-left: 200px;
-            margin-right: 200px;
+            margin-left: 400px;
+            margin-right: 400px;
             border-radius: 3px;
 
         }
 
-        .estonian {
 
+        .estonian_box {
+
+            display: flex;
+            justify-content: space-around;
             font-family: "Kumbh Sans", sans-serif;
             font-optical-sizing: auto;
             font-weight: 500;
             font-style: normal;
             font-variation-settings:
             "YOPQ" 300;
-            text-align: center;
-            word-spacing: 80px;
+            text-align: left;
             border: 0.5px;
             border-style: solid;
             padding: 20px;
             margin-top: 10px;
-            margin-left: 233px;
-            margin-right: 200px;
+            margin-left: 400px;
+            margin-right: 400px;
             border-radius: 3px;
 
         }
 
-        .physics {
+        .physics_box {
 
+            display: flex;
+            justify-content: space-around;
             font-family: "Kumbh Sans", sans-serif;
             font-optical-sizing: auto;
             font-weight: 500;
-            font-style: normal;
+            font-style: left;
             font-variation-settings:
             "YOPQ" 300;
-            text-align: center;
-            word-spacing: 80px;
+            text-align: left;
             border: 0.5px;
             border-style: solid;
             padding: 20px;
             margin-top: 10px;
-            margin-left: 243px;
-            margin-right: 200px;
+            margin-left: 400px;
+            margin-right: 400px;
             border-radius: 3px;
 
         }
+
+        .biology_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .english_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .russian_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .literature_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .pe_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .design_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .programming_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .database_box {
+
+            display: flex;
+            justify-content: space-around;
+            font-family: "Kumbh Sans", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: left;
+            font-variation-settings:
+            "YOPQ" 300;
+            text-align: left;
+            border: 0.5px;
+            border-style: solid;
+            padding: 20px;
+            margin-top: 10px;
+            margin-left: 400px;
+            margin-right: 400px;
+            border-radius: 3px;
+
+        }
+
+        .bottom_page {
+
+
+            font-family: "Manrope", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 400;
+            font-size: 14px;
+            display: flex;
+            flex-direction: column;
+            margin-left: 500px;
+            margin-right: 500px;
+            margin-top: 50px;
+
+        }
+
+        .submit_button {
+
+            margin-left:200px;
+            margin-right:200px;
+            margin-top: 20px;
+            padding: 10px;
+            border-radius: 30px;
+            border-color: #2E81FF;
+
+        }
+    
+
 
     </style>
 
@@ -110,7 +319,7 @@
 
     <div class="heading">
 
-        <h1> Please give feedback for the courses you have taken, so that the school can improve its teaching methods and content! </h1>
+        <h> Please give feedback for the courses you have taken, so that the school can improve its teaching methods and content! </h>
 
     </div>
 
@@ -124,7 +333,17 @@
         $group = test_input($_POST["group"]);
         $email = test_input($_POST["email"]);
         $comment = test_input($_POST["comment"]);
-        $choise = test_input($_POST["choise"]);
+        $math_choise = test_input($_POST["math_choise"]);
+        $est_choise= test_input($_POST["est_choise"]);
+        $phys_choise = test_input($_POST["phys_choise"]);
+        $bio_choise = test_input($_POST["bio_choise"]);
+        $eng_choise = test_input($_POST["eng_choise"]);
+        $rus_choise = test_input($_POST["rus_choise"]);
+        $lit_choise = test_input($_POST["lit_choise"]);
+        $pe_choise = test_input($_POST["pe_choise"]);
+        $des_choise = test_input($_POST["des_choise"]);
+        $prog_choise = test_input($_POST["prog_choise"]);
+        $db_choise = test_input($_POST["db_choise"]);
 
     }
 
@@ -139,67 +358,194 @@
 
 ?>
 
-    <h2 class="second_heading">Poor Bad Okay Good Excellent</h2>
+    <div class="second_heading">
+        <div class="poor_name">
+            Poor
+        </div>
+
+        <div class="bad_name">
+            Bad
+        </div>
+
+        <div class="okay_name">
+            Okay
+        </div>
+
+        <div class="good_name">
+            Good
+        </div>
+
+        <div class="excellent_name">
+            Excellent
+        </div>
+    </div>
+
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
 
-        <div class="math">
+        <div class="math_box">
 
-            Mathematics
-                
-            <input type="radio" name="choise" value="Poor">
-            <input type="radio" name="choise" value="Bad">
-            <input type="radio" name="choise" value="Okay">
-            <input type="radio" name="choise" value="Good">
-            <input type="radio" name="choise" value="Excellent">
+            Mathematics&thinsp;&thinsp;
+
+            <input type="radio" name="math_choise" value="Poor">
+            <input type="radio" name="math_choise" value="Bad">
+            <input type="radio" name="math_choise" value="Okay">
+            <input type="radio" name="math_choise" value="Good">
+            <input type="radio" name="math_choise" value="Excellent">
     
         </div>
 
-        <div class="estonian">
+        <div class="estonian_box">
 
-            Estonian
+            Estonian&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
                 
-            <input type="radio" name="choise" value="Poor">
-            <input type="radio" name="choise" value="Bad">
-            <input type="radio" name="choise" value="Okay">
-            <input type="radio" name="choise" value="Good">
-            <input type="radio" name="choise" value="Excellent">
+            <input type="radio" name="est_choise" value="Poor">
+            <input type="radio" name="est_choise" value="Bad">
+            <input type="radio" name="est_choise" value="Okay">
+            <input type="radio" name="est_choise" value="Good">
+            <input type="radio" name="est_choise" value="Excellent">
     
         </div>
 
-        <div class="physics">
+        <div class="physics_box">
 
-            Physics
+            Physics&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
                 
-            <input type="radio" name="choise" value="Poor">
-            <input type="radio" name="choise" value="Bad">
-            <input type="radio" name="choise" value="Okay">
-            <input type="radio" name="choise" value="Good">
-            <input type="radio" name="choise" value="Excellent">
+            <input type="radio" name="phys_choise" value="Poor">
+            <input type="radio" name="phys_choise" value="Bad">
+            <input type="radio" name="phys_choise" value="Okay">
+            <input type="radio" name="phys_choise" value="Good">
+            <input type="radio" name="phys_choise" value="Excellent">
     
         </div>
 
+        <div class="biology_box">
 
+            Biology&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+                
+            <input type="radio" name="bio_choise" value="Poor">
+            <input type="radio" name="bio_choise" value="Bad">
+            <input type="radio" name="bio_choise" value="Okay">
+            <input type="radio" name="bio_choise" value="Good">
+            <input type="radio" name="bio_choise" value="Excellent">
 
+        </div>  
 
+        <div class="english_box">
 
+            English&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+                
+            <input type="radio" name="eng_choise" value="Poor">
+            <input type="radio" name="eng_choise" value="Bad">
+            <input type="radio" name="eng_choise" value="Okay">
+            <input type="radio" name="eng_choise" value="Good">
+            <input type="radio" name="eng_choise" value="Excellent">
 
+        </div>  
+
+        <div class="russian_box">
+
+            Russian&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+                
+            <input type="radio" name="rus_choise" value="Poor">
+            <input type="radio" name="rus_choise" value="Bad">
+            <input type="radio" name="rus_choise" value="Okay">
+            <input type="radio" name="rus_choise" value="Good">
+            <input type="radio" name="rus_choise" value="Excellent">
+
+        </div>  
+
+        <div class="literature_box">
+
+            Literature&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+                
+            <input type="radio" name="lit_choise" value="Poor">
+            <input type="radio" name="lit_choise" value="Bad">
+            <input type="radio" name="lit_choise" value="Okay">
+            <input type="radio" name="lit_choise" value="Good">
+            <input type="radio" name="lit_choise" value="Excellent">
+
+        </div>  
+
+        <div class="pe_box">
+
+            PE&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+
+            <input type="radio" name="pe_choise" value="Poor">
+            <input type="radio" name="pe_choise" value="Bad">
+            <input type="radio" name="pe_choise" value="Okay">
+            <input type="radio" name="pe_choise" value="Good">
+            <input type="radio" name="pe_choise" value="Excellent">
+
+        </div>  
+
+        <div class="design_box">
+
+            Design&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+                
+            <input type="radio" name="des_choise" value="Poor">
+            <input type="radio" name="des_choise" value="Bad">
+            <input type="radio" name="des_choise" value="Okay">
+            <input type="radio" name="des_choise" value="Good">
+            <input type="radio" name="des_choise" value="Excellent">
+
+        </div>  
+
+        <div class="programming_box">
+
+            Programming
+                
+            <input type="radio" name="prog_choise" value="Poor">
+            <input type="radio" name="prog_choise" value="Bad">
+            <input type="radio" name="prog_choise" value="Okay">
+            <input type="radio" name="prog_choise" value="Good">
+            <input type="radio" name="prog_choise" value="Excellent">
+
+        </div>  
+
+        <div class="database_box">
+
+            Databases&thinsp;&thinsp;&thinsp;&thinsp;&thinsp;
+            
+            <input type="radio" name="db_choise" value="Poor">
+            <input type="radio" name="db_choise" value="Bad">
+            <input type="radio" name="db_choise" value="Okay">
+            <input type="radio" name="db_choise" value="Good">
+            <input type="radio" name="db_choise" value="Excellent">
+
+        </div>  
 
         <br><br>
 
-        Group: <input type="text" name="group">
+        <div class="bottom_page">
 
-        <br><br>
+            <div class="mb-3">
 
-        E-mail: <input type="text" name="email">
+                <label for="exampleFormControlInput1" class="form-label"> &emsp;Group </label>
 
-        <br><br>
+                <input name="group" type="text" class="form-control" id="exampleFormControlInput1">
 
-        Comment: <textarea name="comment" rows="5" cols="40"></textarea>
+            </div>
 
-        <br><br>
+            <div class="mb-3">
 
-        <input type="submit" name="submit" value="Submit">  
+                <label for="exampleFormControlInput1" class="form-label"> &emsp;Email </label>
+
+                <input type="email" type="text" class="form-control" id="exampleFormControlInput1">
+
+            </div>
+
+            <div class="mb-3">
+
+                <label for="exampleFormControlTextarea1" class="form-label"> &emsp;Additional Comments </label>
+
+                <textarea name="comment" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+
+            </div>
+
+            <input class="submit_button" type="submit" name="submit" value="Submit"> 
+
+        </div>
 
     </form>
 
@@ -212,10 +558,31 @@
     echo "<br>";
     echo $comment;
     echo "<br>";
-    echo $choise;
+    echo $math_choise;
+    echo "<br>";
+    echo $est_choise;
+    echo "<br>";
+    echo $phys_choise;
+    echo "<br>";
+    echo $bio_choise;
+    echo "<br>";
+    echo $eng_choise;
+    echo "<br>";
+    echo $rus_choise;
+    echo "<br>";
+    echo $lit_choise;
+    echo "<br>";
+    echo $pe_choise;
+    echo "<br>";
+    echo $des_choise;
+    echo "<br>";
+    echo $prog_choise;
+    echo "<br>";
+    echo $db_choise;
 
 ?>
 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </body>
 </html>
