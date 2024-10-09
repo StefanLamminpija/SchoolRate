61f3567a56d2326ca521fff06c0fa7ab76b2f6a8
Ervireb <77449663+Ervireb@users.noreply.github.com>
2024-10-09 11:44:48 +0300

    Update connect.php
    edited log

--- connect.php
+++ connect.php
@@ -8,9 +8,10 @@ $database = "schoolrateDB";
 $mysqli = new mysqli($host, $username, $password, $database);
 
 if ($mysqli->connect_error) {
-    die ("Connection failed: " . $mysqli->connect_error);
+    error_log("Connection failed: " . $mysqli->connect_error);
+    die ("Connection failed");
 } else {
-    echo "Data has been Sent! ";
+    echo "Data has been Sent!";
 }
 
 $mysqli->set_charset("utf8");
